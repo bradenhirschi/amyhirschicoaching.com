e087a7c7538873a515126855c6c8fb075b42b561
Braden Hirschi <bradenhirschi29@gmail.com>
2023-09-30 11:52:07 -0500

    Updated hero section to a more modern look and improved responsiveness

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import amyInItaly from "/public/amy-in-italy.jpg";
 import familyPhoto from "/public/familyPhoto.jpeg";
-import background from "/public/background.jpeg";
+import background from "/public/background-crop.jpeg";
 import CallToActionButton from "@component/components/callToActionButton";
 import "./styles.css";
 import layeredWaves from "/public/layered-waves-haikei.svg";
@@ -30,20 +30,28 @@ function LearnMoreCard({
 export default function Home() {
   return (
     <>
-      <div className="relative">
-        <Image src={background} alt="background" className="w-full" priority />
-        <div className="bg-gray-800 bg-opacity-60 sm:grid sm:grid-cols-1 lg:grid-cols-2 p-8 sm:p-48 sm:absolute sm:top-0 sm:left-0 sm:h-full">
-          <div className="flex flex-col pr-12">
-            <h1 className="text-white font-serif font-thin mb-4">
-              Get all the tools you need to unlock your potential in EVERY area
-              of your life
+      <section className="grid grid-cols-3 relative pb-[85px]">
+        <div className="hidden lg:block" />
+        <Image
+          src={background}
+          alt="background"
+          className="col-span-3 lg:col-span-2 filter brightness-75"
+          priority
+        />
+        <div className="col-span-3 w-full lg:w-auto lg:max-w-xl xl:max-w-2xl py-20 pl-16 pr-24 xl:pl-32 xl:pr-44 lg:absolute  xl:top-16 bg-gray-100">
+          <div className="flex flex-col">
+            <h1 className="font-serif font-thin mb-4 ">
+              Schedule a free consultation today
             </h1>
+            <h3 className="font-serif font-extralight mb-4">
+              Get the tools you need to unlock your potential!
+            </h3>
             <CallToActionButton />
           </div>
         </div>
-      </div>
+      </section>
 
-      <section className="bg-gray-100 p-8 sm:px-80 sm:py-24 text-center">
+      <section className="bg-gray-100 py-8 px-12 sm:px-24 lg:px-80 md:py-12 lg:py-24 text-center">
         <h3 className="font-serif italic font-thin mb-4">
           “The joy we feel has little to do with the circumstances of our lives
           and everything to do with the focus of our lives.”
@@ -51,10 +59,10 @@ export default function Home() {
         <h4>- Russell M. Nelson</h4>
       </section>
 
-      <section className="flex flex-col p-8 sm:px-48 sm:py-12 items-center text-center">
+      <section className="flex flex-col p-8 sm:px-12 lg:px-48 sm:py-12 items-center text-center">
         <h1 className="text-3xl font-bold mb-6">ABOUT ME</h1>
         <div className="h-[4px] w-[80px] bg-dark-gold mb-12"></div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 sm:gap-24">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-24">
           <LearnMoreCard
             title={"Hi, I'm Amy"}
             description={
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,19 +1,17 @@
 import ahLogo from "../../public/ah-logo.png";
 import Image from "next/image";
-import CallToActionButton from "@component/components/callToActionButton";
 import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex h-[65px] items-center px-4 md:px-20 lg:px-32">
-      <div className="flex flex-grow">
-        <Link href="/">
-          <Image src={ahLogo} alt="AH logo" height={50} />
-        </Link>
-      </div>
+    <nav className="flex h-[85px] items-center px-4 md:px-20 lg:px-32">
+      <Link href="/">
+        <Image src={ahLogo} alt="AH logo" height={50} />
+      </Link>
+      <div className="grow" />
       <div>
-        <div className="sm:hidden">Menu</div>
-        <ul className="hidden sm:flex flex-row gap-8">
+        <div className="hidden">Menu</div>
+        <ul className="flex-row gap-8">
           <li className="hidden">
             <Link href="./about" className="navbar-link">
               About
