fd5073abb1583385933679336afd53e5128511e9
Braden Hirschi <bradenhirschi29@gmail.com>
2023-10-25 13:23:43 -0500

    Added social media links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,19 @@
+import { BsFacebook, BsInstagram } from "react-icons/bs";
+
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-center pt-12 pb-24">
-      <p className="text-white">Copyright © 2023 Amy Hirschi</p>
+    <footer className="bg-gray-800 flex flex-col items-center pt-12 pb-24 text-gray-100">
+      <div>
+        <nav className="flex social-links mb-4 justify-center gap-4">
+          <a href="https://www.instagram.com/the_adoptioncoach">
+            <BsInstagram size={24} />
+          </a>
+          <a href="https://www.facebook.com/theadoptioncoach">
+            <BsFacebook size={24} />
+          </a>
+        </nav>
+        <p>Copyright © 2023 Amy Hirschi</p>
+      </div>
     </footer>
-  )
+  );
 }
